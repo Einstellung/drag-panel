158d1449db9ce5e2ba1781d915d31929c065917d
huangxy1 <huangxy1@xiaopeng.com>
2022-09-21 10:47:43 +0800

    text onduble click function

--- src/ui/components/localComponents/TextInput.tsx
+++ src/ui/components/localComponents/TextInput.tsx
@@ -1,15 +1,41 @@
+import { useState } from "react"
 import styles from "./component.module.scss"
 
 export const TextInput = ({text}: {
   text: string
 }) => {
+
+  const [state, setState] = useState({text: text, state: "display"})
+
   return (
     <div
-      className={styles['txt-input']} 
+      className={styles['txt-input']}
+      onDoubleClick={e => {
+        setState(x => {
+          return {...x, state: "edit"}
+        })
+      }}
     >
-      <span>
+      <span style={{
+        display: state.state === "display" ? "block" : "none"
+      }}>
         {text}
       </span>
+      <input
+        style={{
+          display: state.state === "edit" ? "block" : "none"
+        }}
+        onChange={e => {
+          setState(x => {
+            return {...x, text: e.target.value}
+          })
+        }}
+        onBlur={e => {
+          setState(x => {
+            return {...x, state: "display"}
+          })
+        }}
+      />
     </div>
   )
 }
